d6e9ab8c28d60c5e31a58dd6d4e33876b3cf1fdb
closing <zhu.yuxing@hotmail.com>
2018-11-12 17:00:08 +0900

    Create SpringSecurityStudyApplication.java

---
+++ src/main/java/com/example/demo/SpringSecurityStudyApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityStudyApplication.class, args);
+	}
+}
